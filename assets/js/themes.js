7dd5a4451d868b3771d92e4355ad51fda35fc3b1
Luckie <106387809+LuckieCrab@users.noreply.github.com>
2022-11-14 14:49:26 +0100

    Delete assets directory

--- assets/js/themes.js
+++
@@ -1,5 +0,0 @@
-var body = document.body, html = document.documentElement;
-
-var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight )
-
-body.style.setProperty('--header-height', height - document.querySelector(".nav").offsetHeight + "px")

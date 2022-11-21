ddfd68b800140eb943898de24cabc3011acba731
LuckieCrab <luckie@luckiecrab.nl>
2022-11-21 21:33:25 +0100

    finish feedback

---
+++ assets/js/endfeedback.js
@@ -0,0 +1,20 @@
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+}
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+setCookie('feedback', 'true');
